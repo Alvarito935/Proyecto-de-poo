f19bcc29397a43b4ae9fae67544ab0c1706be962
USUARIO <USUARIO@DESKTOP-12TU5N0>
2026-05-24 16:33:32 -0500

    anghelo chipi

--- src/proyecto_poo/Proyecto_POO.java
+++ src/proyecto_poo/Proyecto_POO.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package proyecto_poo;
 
 /**
@@ -10,11 +6,15 @@ package proyecto_poo;
  */
 public class Proyecto_POO {
 
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        System.out.println("anghelo chipi");
         // TODO code application logic here
     }
+ 
     
 }
